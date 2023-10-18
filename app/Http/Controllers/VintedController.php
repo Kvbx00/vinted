1a2732e905def582ea0b1352b2cdfdca673e87cc
Jakub Pawłowski <kubapawlowski98@gmail.com>
2023-10-18 11:57:52 +0200

    added products from session

--- app/Http/Controllers/VintedController.php
+++ app/Http/Controllers/VintedController.php
@@ -2,59 +2,57 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class VintedController extends Controller
 {
-    public function searchProducts(Request $request)
+    public function searchProducts()
     {
         $timeoutInSeconds = 120;
         $maxRedirects = 10;
 
-        $cookie = $this->getCookie("https://www.vinted.fr/");
+        // Sprawdź, czy produkty są już zapisane w sesji
+        if (!session()->has('products')) {
+            $cookie = $this->getCookie("https://www.vinted.fr/");
+            $response = Http::withHeaders([
+                'Cookie' => '_vinted_fr_session=' . $cookie,
+            ])->timeout($timeoutInSeconds)->maxRedirects($maxRedirects)->get('https://www.vinted.pl/api/v2/catalog/items');
 
-        $response = Http::withHeaders([
-            'Cookie' => '_vinted_fr_session=' . $cookie,
-        ])->timeout($timeoutInSeconds)->maxRedirects($maxRedirects)->get('https://www.vinted.pl/api/v2/catalog/items', [
-            'search_text' => $request->input('search_text'),
-            'page' => $request->input('page', 1),
-            'per_page' => 24,
-        ]);
+            // Zapisz pobrane produkty w sesji, ale tylko jeśli udało się je pobrać
+            if ($response->successful()) {
+                session(['products' => $response->json()]);
+            } else {
+                // Jeśli pobieranie produktów nie powiodło się, obsłuż ten przypadek
+                return view('error.view'); // Stwórz widok błędu
+            }
+        }
 
-        $products = $response->json();
+        // Pobierz produkty z sesji
+        $products = session('products');
 
-        $sortOption = $request->input('sort_by');
+        // Sortowanie według preferencji użytkownika
+        $sortowanie = request('sortowanie', 'default'); // Domyślnie sortuj wg. oryginalnego porządku
 
-        // Sortowanie produktów
-        if ($sortOption === 'price_asc') {
-            usort($products['items'], function($a, $b) {
-                return floatval($a['price']) - floatval($b['price']);
+        if ($sortowanie === 'nazwa') {
+            usort($products['items'], function ($a, $b) {
+                return strcmp($a['brand_title'], $b['brand_title']);
             });
-        } elseif ($sortOption === 'price_desc') {
-            usort($products['items'], function($a, $b) {
-                return floatval($b['price']) - floatval($a['price']);
-            });
-        } elseif ($sortOption === 'favorite_asc') {
-            usort($products['items'], function($a, $b) {
-                return intval($a['favourite_count']) - intval($b['favourite_count']);
-            });
-        } elseif ($sortOption === 'favorite_desc') {
-            usort($products['items'], function($a, $b) {
-                return intval($b['favourite_count']) - intval($a['favourite_count']);
+        } elseif ($sortowanie === 'cena') {
+            usort($products['items'], function ($a, $b) {
+                return $a['price'] - $b['price'];
             });
         }
 
-        $perPage = 24;
-        $currentPage = $request->input('page', 1);
-        $totalItems = count($products['items']);
-        $products = array_slice($products['items'], ($currentPage - 1) * $perPage, $perPage);
-        $products = new \Illuminate\Pagination\LengthAwarePaginator($products, $totalItems, $perPage, $currentPage);
+        // Paginacja
+        $produktyNaStronie = 10; // Określ ilość produktów na jednej stronie
+        $currentPage = request('page', 1);
+        $pagedProducts = array_slice($products['items'], ($currentPage - 1) * $produktyNaStronie, $produktyNaStronie);
 
         return view('products.index', [
-            'products' => $products,
+            'products' => $pagedProducts,
+            'currentPage' => $currentPage,
+            'totalPages' => ceil(count($products['items']) / $produktyNaStronie),
         ]);
-    
     }
 
     private function getCookie($url)
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,85 +11,76 @@
 <header class="vinted-header">
     <div class="container">
         <div class="logo-bar">
-        <a href="/">
-            <img src="https://static.vinted.com/assets/web-logo/default/logo.svg" alt="Vinted logo" class="logo">
-        </a>
-        </div>
-        <div class="search-bar">
-            <form action="{{ route('search.products') }}" method="get" class="form-group" id="form-group">
-                <input type="text" class="form-control" id="search_text" name="search_text" placeholder="Wyszukaj produkty...">
-            </form>
+            <a href="/">
+                <img src="https://static.vinted.com/assets/web-logo/default/logo.svg" alt="Vinted logo" class="logo">
+            </a>
         </div>
     </div>
 </header>
-
+<form id="sortForm" action="{{ route('search.products') }}" method="get">
+    <label for="sortowanie">Sortuj według:</label>
+    <select id="sortowanie" name="sortowanie">
+        <option value="default" {{ request('sortowanie') === 'default' ? 'selected' : '' }}>Domyślne</option>
+        <option value="nazwa" {{ request('sortowanie') === 'nazwa' ? 'selected' : '' }}>Nazwa</option>
+        <option value="cena" {{ request('sortowanie') === 'cena' ? 'selected' : '' }}>Cena</option>
+    </select>
+    <button type="submit">Sortuj</button>
+</form>
 
 <main class="container">
 
-    <div class="row">
-        <div class="sort">
-            <form action="{{ route('search.products') }}" method="get" class="form-group" id="sortForm">
-                <select class="form-control selectpicker" id="sort_by" name="sort_by">
-                    <option value="price_asc">Cena rosnąco</option>
-                    <option value="price_desc">Cena malejąco</option>
-                    <option value="favorite_asc">Ulubione rosnąco</option>
-                    <option value="favorite_desc">Ulubione malejąco</option>
-                </select>
-            </form>
-        </div>
-
-
-
-        <div class="col-12">
-            <div class="product-container">
-                @foreach ($products as $product)
-                    <div class="product-item">
-                        <div class="product-user">
-                            @if(isset($product['user']['photo']['thumbnails'][4]['url']))
-                            <img src="{{ $product['user']['photo']['thumbnails'][4]['url'] }}" alt="{{ $product['brand_title'] }} {{ $product['title'] }}" id="user-photo">
-                            @else
-                                <img id="user-photo">
-                            @endif
-                            <a id="user-nick">{{ $product['user']['login']}}</a>
-                        </div>
-                        <div class="product-image">
-                            <a href="{{ $product['url'] }}"><img src="{{ $product['photo']['thumbnails'][1]['url'] }}" alt="{{ $product['brand_title'] }} {{ $product['title'] }}" ></a>
-                        </div>
-                        <div class="product-details">
-                            <div class="details-row">
-                                <p class="price">{{ $product['price'] }} {{ $product['currency'] }}</p>
-                                <div id="fav2">
+    <div class="col-12">
+        <div class="product-container">
+            @foreach ($products as $product)
+                <div class="product-item">
+                    <div class="product-user">
+                        @if(isset($product['user']['photo']['thumbnails'][4]['url']))
+                            <img src="{{ $product['user']['photo']['thumbnails'][4]['url'] }}"
+                                 alt="{{ $product['brand_title'] }} {{ $product['title'] }}" id="user-photo">
+                        @else
+                            <img id="user-photo">
+                        @endif
+                        <a id="user-nick">{{ $product['user']['login']}}</a>
+                    </div>
+                    <div class="product-image">
+                        <a href="{{ $product['url'] }}"><img src="{{ $product['photo']['thumbnails'][1]['url'] }}"
+                                                             alt="{{ $product['brand_title'] }} {{ $product['title'] }}"></a>
+                    </div>
+                    <div class="product-details">
+                        <div class="details-row">
+                            <p class="price">{{ $product['price'] }} {{ $product['currency'] }}</p>
+                            <div id="fav2">
                                 <div id="fav" class="favorite-icon">
                                     <svg id="heart" viewBox="0 0 16 16" aria-label="Add to favourites">
-                                        <path d="M11.73 1C9.58 1 8 2.74 8 2.74S6.42 1 4.27 1c-.7 0-1.48.2-2.28.7-2.92 1.88-2.47 5.86.34 8.5A83.4 83.4 0 0 0 8 15s3.04-2.32 5.67-4.8c2.81-2.64 3.26-6.62.34-8.5-.8-.5-1.57-.7-2.28-.7m0 1.5c.5 0 .98.15 1.47.47.77.5 1.2 1.2 1.29 2.07.12 1.33-.59 2.88-1.85 4.07A79.25 79.25 0 0 1 8 13.1a79.1 79.1 0 0 1-4.64-3.98C2.1 7.93 1.39 6.37 1.51 5.05c.09-.89.52-1.59 1.3-2.08.48-.32.96-.47 1.46-.47 1.44 0 2.62 1.25 2.62 1.25L8 4.97l1.1-1.22c.02-.01 1.21-1.25 2.63-1.25"></path>
+                                        <path
+                                            d="M11.73 1C9.58 1 8 2.74 8 2.74S6.42 1 4.27 1c-.7 0-1.48.2-2.28.7-2.92 1.88-2.47 5.86.34 8.5A83.4 83.4 0 0 0 8 15s3.04-2.32 5.67-4.8c2.81-2.64 3.26-6.62.34-8.5-.8-.5-1.57-.7-2.28-.7m0 1.5c.5 0 .98.15 1.47.47.77.5 1.2 1.2 1.29 2.07.12 1.33-.59 2.88-1.85 4.07A79.25 79.25 0 0 1 8 13.1a79.1 79.1 0 0 1-4.64-3.98C2.1 7.93 1.39 6.37 1.51 5.05c.09-.89.52-1.59 1.3-2.08.48-.32.96-.47 1.46-.47 1.44 0 2.62 1.25 2.62 1.25L8 4.97l1.1-1.22c.02-.01 1.21-1.25 2.63-1.25"></path>
                                     </svg>
                                 </div>
                                 <p class="favorites">{{ $product['favourite_count'] }}</p>
-                                </div>
                             </div>
-
-                            <div class="details-row2">
-                                <p class="price-fee">{{ $product['total_item_price'] }}  zł, w tym</p>
-                                    <div id="fav" class="favorite-icon">
-                                        <svg id="shield" viewBox="0 0 16 16"><path d="m7.34 8.3 3.56-3.58 1.07 1-4.62 4.67-2.89-2.82 1.06-1Zm7.33-5.9V8c0 5.6-6.67 8-6.67 8s-6.67-2.4-6.67-8V2.4L8 0Zm-1.5 1.05L8 1.59 2.83 3.45V8a6.31 6.31 0 0 0 2.76 4.92 13.09 13.09 0 0 0 2 1.28l.37.17.37-.17a13.09 13.09 0 0 0 2-1.28A6.31 6.31 0 0 0 13.17 8Z"></path></svg>
-                                    </div>
-                            </div>
-
-                            <p class="size"> {{ $product['size_title'] }}</p>
-                            <p class="brand_title"> {{ $product['brand_title'] }}</p>
                         </div>
+
+                        <div class="details-row2">
+                            <p class="price-fee">{{ $product['total_item_price'] }} zł, w tym</p>
+                            <div id="fav" class="favorite-icon">
+                                <svg id="shield" viewBox="0 0 16 16">
+                                    <path
+                                        d="m7.34 8.3 3.56-3.58 1.07 1-4.62 4.67-2.89-2.82 1.06-1Zm7.33-5.9V8c0 5.6-6.67 8-6.67 8s-6.67-2.4-6.67-8V2.4L8 0Zm-1.5 1.05L8 1.59 2.83 3.45V8a6.31 6.31 0 0 0 2.76 4.92 13.09 13.09 0 0 0 2 1.28l.37.17.37-.17a13.09 13.09 0 0 0 2-1.28A6.31 6.31 0 0 0 13.17 8Z"></path>
+                                </svg>
+                            </div>
+                        </div>
+
+                        <p class="size"> {{ $product['size_title'] }}</p>
+                        <p class="brand_title"> {{ $product['brand_title'] }}</p>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-12">
-            <nav class="pagination">
-                {{ $products->appends(request()->input())->links() }}
-            </nav>
-        </div>
+    <div class="pagination">
+        @for ($i = 1; $i <= $totalPages; $i++)
+            <a href="{{ route('search.products', ['page' => $i, 'sortowanie' => request('sortowanie')]) }}" class="{{ $currentPage == $i ? 'active' : '' }}">{{ $i }}</a>
+        @endfor
     </div>
 </main>
 
@@ -97,10 +88,10 @@
 </body>
 
 <script>
-    document.getElementById('sort_by').addEventListener('change', function() {
+    document.getElementById('sort_by').addEventListener('change', function () {
         document.getElementById('sortForm').submit();
     });
-    document.getElementById('search_text').addEventListener('keydown', function(event) {
+    document.getElementById('search_text').addEventListener('keydown', function (event) {
         if (event.key === 'Enter') {
             event.preventDefault();
             document.getElementById('form-group').submit();
